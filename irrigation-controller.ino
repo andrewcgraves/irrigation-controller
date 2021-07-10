59475a9768c0fa0f9d0976e671ff27c9e8be534b
Andrew <andrewcgraves@gmail.com>
2021-07-10 12:20:55 -0700

    pushing bairly working code

--- irrigation-controller.ino
+++ irrigation-controller.ino
@@ -1,31 +1,35 @@
 #include <WiFiNINA.h>
 #include <ezButton.h>
 #include <Adafruit_NeoPixel.h>
+#include <TimeLib.h>
+#include <SPI.h>
 
 #include "arduino_secrets.h"
 
 // Led Config Stuff
 #define LED_PIN 4
 #define LED_COUNT 1
-Adafruit_NeoPixel leds = Adafruit_NeoPixel(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // Network Config Information
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 int WifiRadioStatus = WL_IDLE_STATUS;     // the WiFi radio's status
 WiFiServer wifiServer(80);
-WiFiClient wifiClient;
 
 // Vars
 long WATERING_LENGTH_MIN = 10;
+int NUMBER_OF_WATERING_ZONES = 3;
 long controllerStartTime;
 
 long wateringStartTime;
 long wateringEndTime;
 long lastWateringTime;
+long int secondsSinceProgramming = -1;
 
 bool isAutomatic;
 bool isWatering;
+bool timeReset = false;
 
 int lastZone;
 int currentZone;
@@ -39,8 +43,6 @@ int zone4Pin = 13;
 // Setup for Buttons
 ezButton manualButton(7);
 ezButton autoButton(8);
-SerLCD lcd;
-
 
 void setup() {
     leds.begin();
@@ -53,21 +55,29 @@ void setup() {
     pinMode(zone3Pin, OUTPUT);
 
     setLedColor(0xE34C00, 10);
-    Serial.begin(96000);
-    wifiClient = wifiServer.available();
+    Serial.begin(9600);
+//    wifiClient = wifiServer.available();
 
-    while (!Serial) {
-        // wait for serial port to connect
-    }
+//    while (!Serial) {
+//        // wait for serial port to connect
+//    }
 
-    connectToWifi()
-    getCurrentTime()
+    connectToWifi();
+    getCurrentTime();
+    endWatering();
+    wifiServer.begin();
     Serial.println("Setup finished!");
+    setLedColor(0xFFFFFF, 10);
 }
 
 void loop() {
     manualButton.loop();
     autoButton.loop();
+    WiFiClient wifiClient = wifiServer.available();
+
+//    if (wifiClient) {
+//        Serial.println("New Client!!!");
+//    }
 
     // Check to see if the minute hour and second are at the hour and reset the time accordingly
     // This aims to keep the time accurate because it will slowly fall behind
@@ -76,31 +86,90 @@ void loop() {
         timeReset = true;
     }
 
-    if (second() == 1) { timeReset = false }
-
+    if (second() == 1) {
+      timeReset = false;
+    }
 
     // TRIGGERING CHECKS
     if (manualButton.isPressed()) {
+        endWatering();
+        triggerAuto(false);
+        isAutomatic = false;
+        currentZone >= 3 ? currentZone = 1 : currentZone += 1;
+        secondsSinceProgramming = millis() / 1000;
 
     } else if (autoButton.isPressed()) {
+        endWatering();
+
+        if (isAutomatic) {
+            triggerAuto(false);
+            // isAutomatic = false;
+            Serial.println("Automatic off");
+
+        } else {
+            triggerAuto(true);
+            Serial.println("Automatic on");
+        }
 
     } else if (second() == 0 && minute() == 0 && hour() == 21 && isAutomatic && !isWatering) {
-        
+        Serial.println("Schedule triggered");
+        endWatering();
+        currentZone = 1;
+        triggerWatering();
+
     } else if (wifiClient) {
         Serial.println("new client");
         String req = wifiClient.readStringUntil('\r');
 
-        wifiClient.flush()
-        wifiClient.stop()
+        wifiClient.flush();
+        wifiClient.stop();
         Serial.println("client disconnected");
 
         if (req.indexOf("/on") != -1) {
+            int posOfZone = req.indexOf("/on/") + 4;
+            String zone = req.substring(posOfZone, posOfZone + 1);
+            String runTime = req.substring(posOfZone + 2, req.indexOf("HTTP")); 
+            Serial.println("ON... TIME: " + runTime + " | Zone: " + zone);
+
+            currentZone = zone.toInt();
+            WATERING_LENGTH_MIN = runTime.toInt();
+            triggerWatering();
 
         } else if (req.indexOf("/off") != -1) {
+            Serial.println("OFF");
+            triggerAuto(false);
+            // isAutomatic = false;  
 
         } else if (req.indexOf("/auto") != -1) {
-
+            Serial.println("AUTO");
+            int posOfRuntime = req.indexOf("/auto/");
+            String runTime = req.substring(posOfRuntime + 6, req.indexOf("HTTP"));
         }
+
+    }
+
+    // LOOPING THROUGH TO CHECK THE TIME
+    if (isWatering && wateringEndTime <= now()) {
+
+        if (isAutomatic && currentZone < NUMBER_OF_WATERING_ZONES) {
+            Serial.println("Watering Next Zone");
+            endWatering();
+            currentZone = lastZone + 1;
+            triggerWatering();
+
+        } else if (isAutomatic && currentZone >= NUMBER_OF_WATERING_ZONES) {
+            endWatering();
+            Serial.println("back to auto rest state");
+        } else {
+            endWatering();
+            Serial.println("done watering");
+        }
+    }
+
+    if (isWatering == false && secondsSinceProgramming != -1 && (millis() / 1000 ) - secondsSinceProgramming > 5) {
+        secondsSinceProgramming = -1;
+        Serial.println("Watering!");
+        triggerWatering();
     }
 }
 
@@ -108,7 +177,7 @@ void loop() {
 // HELPER FUNCTIONS
 // -------------------------------------------------------------------------
 
-void setLedColor(hex, intensity) {
+void setLedColor(long hex, int intensity) {
     leds.setPixelColor(0, hex); 
     leds.setBrightness(10);
     leds.show();
@@ -117,11 +186,54 @@ void setLedColor(hex, intensity) {
 // WATERING FUNCTIONS
 
 void triggerWatering() {
+    isWatering = true;
+    wateringStartTime = now();
+    wateringEndTime = now() + (WATERING_LENGTH_MIN * 60);
 
+    Serial.print("Watering Zone: ");
+    Serial.println(currentZone);
+
+    setLedColor(0x00FFFF, 10);
+
+    switch (currentZone) {
+        case 1: 
+            digitalWrite(zone1Pin, LOW);
+            break;
+        case 2:
+            digitalWrite(zone2Pin, LOW);
+            break;
+        case 3:
+            digitalWrite(zone3Pin, LOW);
+            break;
+        default:
+            Serial.println("INVALID ZONE");
+            setLedColor(0xFFFFFF, 10);
+    }
+}
+
+void triggerAuto(bool state) {
+
+    if (state) {
+        setLedColor(0x00FF00, 10);
+    } else {
+        setLedColor(0xFFFFFF, 10);
+    }
+
+    isAutomatic = state;
 }
 
 void endWatering() {
+    lastZone = currentZone;
+    lastWateringTime = now();
+    currentZone = 0;
+    isWatering = false;
+    setLedColor(0xFFFFFF, 10);
 
+    // Set all the zones to be off
+    digitalWrite(zone1Pin, HIGH);
+    digitalWrite(zone2Pin, HIGH);
+    digitalWrite(zone3Pin, HIGH);
+    digitalWrite(zone4Pin, HIGH);
 }
 
 // TIME GATHERING 
@@ -161,12 +273,12 @@ void connectToWifi() {
     }
         
     // attempt to connect to WiFi network:
-    while ( status != WL_CONNECTED) {
+    while ( WifiRadioStatus != WL_CONNECTED ) {
         Serial.print("Attempting to connect to WPA SSID: ");
         Serial.println(ssid);
         
         // Connect to WPA/WPA2 network:
-        status = WiFi.begin(ssid, pass);
+        WifiRadioStatus = WiFi.begin(ssid, pass);
         // wait 5 seconds for connection:
         delay(5000);
     }
@@ -174,7 +286,6 @@ void connectToWifi() {
     // you're connected now, so print out the data:
     Serial.println("You're connected to the network");
     printWiFiData();
-    }
 }
 
 void printWiFiData() {
@@ -208,14 +319,10 @@ void printWiFiData() {
     Serial.print("SSID: ");
     Serial.println(WiFi.SSID());
 
-    lcd.clear(); //Clear the display - this moves the cursor to home position as well
-    lcd.print("Connected!");
-
     // print the MAC address of the router you're attached to:
     byte bssid[6];
     WiFi.BSSID(bssid);
     Serial.print("BSSID: ");
-    printMacAddress(bssid);
 
     // print the received signal strength:
     long rssi = WiFi.RSSI();
--- irrigation-controller/arduino_secrets.h
+++
@@ -1,2 +0,0 @@
-#define SECRET_SSID "KG2";
-#define SECRET_PASS "10200abcdefghijklmnopqrstuvwxyz!!!";
